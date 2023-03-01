1277c73649f63630b2816a063833cc887da7f1ca
Illia Chaban <46610176+Illmix@users.noreply.github.com>
2023-03-01 17:10:27 +0100

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import LongPulling from "./LongPulling";
+import EventSourcing from "./EventSourcing";
+import WebSock from "./WebSock";
+
+
+function App() {
+
+  return (
+      <div className="App">
+        <WebSock/>
+      </div>
+  )
+}
+
+export default App
